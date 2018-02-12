17b9431203bd8304a711e239cd525aefe6b00f24
khwanchanok2538 <k_love38@hotmail.com>
2018-02-12 16:51:52 +0700

    User Login Database Connecting

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,27 +1,70 @@
 package sample;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
+import javax.swing.*;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class Controller implements Initializable{
+public class Controller implements Initializable {
     private loginModel loginModel = new loginModel();
     @FXML
     private Label dbStatus;
+    @FXML
+    private TextField username;
+    @FXML
+    private PasswordField password;
+    @FXML
+    private Button btnLogin;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        if (this.loginModel.isDatabaseConnection()){
+        if (this.loginModel.isDatabaseConnection()) {
             this.dbStatus.setText("Connection to DB");
-        }else {
+        } else {
             this.dbStatus.setText("Not Connected to DB");
         }
+    }//initialize
+
+    @FXML
+    public void Login(ActionEvent event) {
+        try {
+            if (this.loginModel.isLogin(this.username.getText(), this.password.getText())) {
+                Stage Stage = (Stage) this.btnLogin.getScene().getWindow();
+                Stage.close();
 
 
+            } else {
+                JOptionPane.showMessageDialog(null, "Your username or password is not corrected.");
+            }
+        } catch (Exception localException) {
+            localException.printStackTrace();
+        }
+    }//Login
 
+    private void adminLogin() {
+        try {
+            Stage adminStage = new Stage();
+            FXMLLoader adminLoader = new FXMLLoader();
+            Pane admininRoot = adminLoader.load(getClass().getClass().getResource("" + "/Admin/AdminDashBoard.fxml")
+                    .openStream());
+        } catch (IOException ex){
+            ex.printStackTrace();
+        }
+
+        JOptionPane.showMessageDialog(null, "Welcome to our System.");
     }
-}//initialize
+
+}
+
 
